c1123709689ff09c748d09e823dc6d6c28f8722f
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2018-03-11 20:53:32 -0700

    Adding the explanation diff in == and ===

---
+++ ===And==.js
@@ -0,0 +1,19 @@
+// == and === are comparison operators
+
+// == just compare value
+if('1' == 1) { // true
+  console.log('true');
+}
+
+if(1 == 1) { // true
+  console.log('true');
+}
+
+// === compare value and type
+if('1' === 1) { // false
+  console.log('true');
+}
+
+if(1 === 1) { // true
+  console.log('true');
+}
