71cbb2d7cfeb40bc49bce8c4272a249ce3974893
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2018-03-11 21:21:28 -0700

    Adding undefined and null explanation

---
+++ nullAndUndefined.js
@@ -0,0 +1,17 @@
+// undefined - declare the variable but no have assigned value
+var x;
+console.log(x); // undefined
+
+let b;
+console.log(b); // undefined
+
+typeof(undefined) // undefined
+
+// null
+typeof(null) // object
+
+var c = null;
+console.log(c); // null
+
+// Example: You can assing null to a variable and
+// wait to the service response if this variable is !null will have response.
